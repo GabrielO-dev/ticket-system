86581165796fe40b9fb2cf7b548bbd502d46ab8e
Gabriel <gabriel.biskup08@gmail.com>
2026-09-18 13:24:03 +0200

    feat: implementacja TicketPoolDao oraz relacji z wydarzeniami.

--- src/main/java/pl/GabrielO/ticketApp/Main.java
+++ src/main/java/pl/GabrielO/ticketApp/Main.java
@@ -1,27 +1,37 @@
 package pl.GabrielO.ticketApp;
 
 import pl.GabrielO.ticketApp.dao.EventDao;
+import pl.GabrielO.ticketApp.dao.TicketPoolDao;
 import pl.GabrielO.ticketApp.model.Event;
+import pl.GabrielO.ticketApp.model.TicketPool;
+
 import java.time.LocalDateTime;
 import java.util.List;
 
 public class Main {
-        public static void main(String[] args) {
-            System.out.println("Uruchamianie systemu biletowego...");
+    public static void main(String[] args) {
+        EventDao eventDao = new EventDao();
+        TicketPoolDao ticketPoolDao = new TicketPoolDao();
 
-            EventDao eventDao = new EventDao();
+        Event event = new Event();
+        event.setName("Koncert rockowy");
+        event.setEventDate(LocalDateTime.of(2026, 11, 20, 19, 0));
+        eventDao.save(event);
 
-            Event rockConcert = new Event();
-            rockConcert.setName("Koncert Rockowy - Warsaw Arena");
-            rockConcert.setEventDate(LocalDateTime.of(2026, 10, 15, 20, 0));
+        Long generatedEventId = event.getId();
 
-            System.out.println("Próba zapisu do bazy...");
-            eventDao.save(rockConcert);
+        TicketPool standardPool = new TicketPool(null, 500, "STANDARD", generatedEventId, 0);
+        TicketPool vipPool = new TicketPool(null, 50, "VIP", generatedEventId, 0);
 
-            System.out.println("\nLista wydarzeń w bazie:");
-            List<Event> allEvents = eventDao.findAll();
-            for (Event e : allEvents) {
-                System.out.println(e.getId() + " | " + e.getName() + " | " + e.getEventDate());
-            }
+        System.out.println("\nZapisywanie biletów do bazy...");
+        ticketPoolDao.save(standardPool);
+        ticketPoolDao.save(vipPool);
+
+        System.out.println("\nPobieranie dostępnych biletów dla koncertu ID " + generatedEventId + ":");
+        List<TicketPool> availablePools = ticketPoolDao.getPoolsForEvent(generatedEventId);
+
+        for (TicketPool pool : availablePools) {
+            System.out.println("- Typ: " + pool.getTicketType() + ", Dostępnych: " + pool.getAvailableTickets() + " (Wersja: " + pool.getVersion() + ")");
         }
+    }
 }
