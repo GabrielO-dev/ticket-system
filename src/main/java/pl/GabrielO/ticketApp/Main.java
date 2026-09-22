5f3954cd356a3dbce856e65ed058913e74d1da09
Gabriel <gabriel.biskup08@gmail.com>
2026-09-22 10:56:20 +0200

    feat: dodanie graficznego interfejsu użytkownika (UI) w Java Swing

--- src/main/java/pl/GabrielO/ticketApp/Main.java
+++ src/main/java/pl/GabrielO/ticketApp/Main.java
@@ -1,25 +1,16 @@
 package pl.GabrielO.ticketApp;
 
-import pl.GabrielO.ticketApp.exception.TicketBookingException;
-import pl.GabrielO.ticketApp.service.TicketBookingService;
+import pl.GabrielO.ticketApp.ui.MainWindow;
+
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        TicketBookingService ticketBookingService = new TicketBookingService();
 
-        Long eventId = 2L;
+        SwingUtilities.invokeLater( () -> {
+            MainWindow window = new MainWindow();
+            window.setVisible(true);
 
-        try {
-            System.out.println("Test 1: Prawidłowy zakup...");
-            ticketBookingService.bookTicket(eventId, "VIP", "Anna Nowak");
-
-            System.out.println("Test 2: Próba zakupu nieistniejącego typu biletu...");
-            ticketBookingService.bookTicket(eventId, "PREMIUM", "Tomasz Kot");
-
-            System.out.println("To się nie powinno wyświetlić.");
-
-        } catch (TicketBookingException e) {
-            System.err.println("PRZECHWYCONO BŁĄD BIZNESOWY: " + e.getMessage());
-        }
+        });
     }
 }
