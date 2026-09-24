2ba04734b367f4ff028de47b4d9f2dc0e26542ab
Gabriel <gabriel.biskup08@gmail.com>
2026-09-24 13:54:11 +0200

    feat: dodanie walidacji formularza rezerwacji

--- src/main/java/pl/GabrielO/ticketApp/dto/BookingRequest.java
+++ src/main/java/pl/GabrielO/ticketApp/dto/BookingRequest.java
@@ -1,9 +1,15 @@
 package pl.GabrielO.ticketApp.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class BookingRequest {
 
     private Long eventId;
     private String ticketType;
+
+    @NotBlank(message = "Imię i nazwisko nie może być puste!")
+    @Size(min = 3, message = "Wpisz co najmniej 3 znaki.")
     private String customerName;
 
     public BookingRequest() {}
